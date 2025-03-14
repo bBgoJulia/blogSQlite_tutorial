ee7017c75b21135f8b4343c22256123ad81aff63
bBgojulia <jborgo968@gmail.com>
2025-03-14 12:23:33 -0300

    helloworsd

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("olá sesi!");
+})
+
+const PORT = 3000; //porta TCP do servidor http da apllicação
+
+app.listen(PORT, ()  =>  {
+    console.log(`Servidor sendo executado na porta ${PORT}!`);
